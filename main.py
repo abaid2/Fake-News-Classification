3a2a87e6707241384dc8f7bde9963c230bfb8943
Aranav Baid <aranav_baid@brown.edu>
2022-05-14 08:10:36 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from webbrowser import BackgroundBrowser
 from preprocess import preprocess
 from model import build_model
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import os
-
 
 def test_model(test, train):
     # Check if the files exist
@@ -21,8 +25,44 @@ def test_model(test, train):
     test_data = pd.read_csv(test)
 
     X, Y, test_data = preprocess(train_data, test_data)
+
+    x_train, X_test, y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42, stratify=Y)
+    X_train, X_valid, Y_train, Y_valid = train_test_split(x_train, y_train, test_size=0.1, random_state=42, stratify=y_train)
+    X_test = test_data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, stratify=Y, random_state=2)
+
     model = build_model()
-    model.fit(X, Y, epochs=20, batch_size=64)
+    history = model.fit(X_train, Y_train, validation_data=(X_valid, Y_valid), epochs=10, batch_size=64) 
+    plot_graphs(history)   
+
+    predictions = np.round(model.predict(X_test).flatten()).astype('int')
+
+    print(classification_report(Y_test, predictions))
+    print(accuracy_score(Y_test, predictions))
+
+
+def plot_graphs(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+
+
+
+
+
 
 
 
@@ -31,6 +71,7 @@ def main():
     train = 'data/train.csv'
     test_model(test, train)
     pass
+
 
 
 if __name__ == "__main__":
--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,14 @@
 import nltk 
 import re
 import numpy as np
+import matplotlib.pyplot as plt
 
 from tensorflow.keras.preprocessing.text import one_hot
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
+
+from wordcloud import WordCloud
 
 vocab_size = 5000
 
@@ -13,6 +16,8 @@ def preprocess(train_data, test_data):
 
     train_data = train_data.fillna('')
     test_data = test_data.fillna('')
+
+    # build_wordcloud(train_data)
 
     train_data['content'] = train_data['title'] + ' ' + train_data['author']
     test_data['content'] = test_data['title'] + ' ' + test_data['author']
@@ -36,14 +41,23 @@ def stem(dataset):
     corpus = []
 
     for i in range(len(dataset)):
-        stem = re.sub('[^a-zA-Z]',' ', dataset['content'][i])
-        stem = stem.lower()
-        stem = stem.split()
-        stem = [ps.stem(word) for word in stem if not word in stopwords.words('english')]
-        stem = ' '.join(stem)
-        corpus.append(stem)
+        seq = re.sub('[^a-zA-Z]',' ', dataset['content'][i])
+        seq = seq.lower()
+        seq = seq.split()
+        seq = [ps.stem(word) for word in seq if not word in stopwords.words('english')]
+        seq = ' '.join(seq)
+        corpus.append(seq)
 
     return corpus
+
+
+def build_wordcloud(dataset):
+    wordcloud = WordCloud(background_color='black', width=500, height=500)
+    textcloud = wordcloud.generate(' '.join(dataset[dataset['label']==1]['text']))
+    plt.figure(figsize=(20, 30))
+    plt.imshow(textcloud)
+    plt.axis('off')
+    plt.show()
 
 
 
